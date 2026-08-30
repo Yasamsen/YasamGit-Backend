e934d157a57006cf4fece89f308d0a5e52c91f8d
ALyaSam <162271637+Yasamsen@users.noreply.github.com>
2026-08-31 06:38:48 +0800

    Update api/session.js

--- api/session.js
+++ api/session.js
@@ -23,39 +23,14 @@ module.exports =
       return;
     }
 
-    try {
-
-      const authenticated =
-        isAuthenticated(req);
-
-      return json(
-        req,
-        res,
-        200,
-        {
-          ok: true,
-          authenticated
-        }
-      );
-
-    } catch (error) {
-
-      console.error(
-        "SESSION_ERROR:",
-        error
-      );
-
-      return json(
-        req,
-        res,
-        500,
-        {
-          ok: false,
-          authenticated:
-            false,
-          error:
-            "Gagal memeriksa session."
-        }
-      );
-    }
+    return json(
+      req,
+      res,
+      200,
+      {
+        ok: true,
+        authenticated:
+          isAuthenticated(req)
+      }
+    );
   };
