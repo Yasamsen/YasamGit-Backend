9872136661eef237a87af380c5d9926ec687385b
ALyaSam <162271637+Yasamsen@users.noreply.github.com>
2026-08-30 20:56:49 +0800

    Update api/health.js

--- api/health.js
+++ api/health.js
@@ -1,8 +1,8 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
   res.status(200).json({
     ok: true,
     service: "YasamGit Backend",
     version: "5.2.0",
     status: "online"
   });
-};
+}
